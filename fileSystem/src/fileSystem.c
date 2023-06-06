9014f660c6ab1408a7b6055210c1691871048eff
Franco Magne <fmagnecolque@frba.utn.edu.ar>
2023-06-06 18:27:57 +0000

    probando

--- fileSystem/src/fileSystem.c
+++ fileSystem/src/fileSystem.c
@@ -5,16 +5,18 @@ t_config* fileSystemConfig;
 
 int main() {
    
-   fileSystemLogger = log_create(FS_LOG_UBICACION,FS_PROCESS_NAME,true,LOG_LEVEL_INFO);
+   fileSystemLogger = log_create(FS_LOG_UBICACION, FS_PROCESS_NAME, true, LOG_LEVEL_INFO);
    fileSystemConfig = config_create(FS_CONFIG_UBICACION);
    char *fileSystemIP = config_get_string_value(fileSystemConfig, "IP");
    char *fileSystemPort = config_get_string_value(fileSystemConfig,"PUERTO_MEMORIA");
 
+
    ///////////////////////////////// CONECTARSE A MEMORIA //////////////////////////////
+
    int fsSocketMemoria = conectar_a_servidor(fileSystemIP, fileSystemPort);
    if (fsSocketMemoria == -1) {
-      log_error(fileSystemLogger, "Error al intentar establecer conexión inicial con módulo Memoria");
 
+      log_error(fileSystemLogger, "Error al intentar establecer conexión inicial con módulo Memoria");
       log_destroy(fileSystemLogger);
 
       return -2;
@@ -23,8 +25,8 @@ int main() {
 
    ///////////////////////////////// CREA SERVIDOR PARA KERNEL /////////////////////////
 
-   int serverFS = iniciar_servidor(fileSystemIP,"8003");
-   log_info(fileSystemLogger,"Servidor kernel listo para recibir al modulo\n");
+   int serverFS = iniciar_servidor(fileSystemIP, "8003");
+   log_info(fileSystemLogger, "Servidor kernel listo para recibir al modulo\n");
    int serverEspera = esperar_cliente(serverFS);
 
 
